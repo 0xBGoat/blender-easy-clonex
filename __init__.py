5f83ec3a80916eb7a8445123c4058c7f937e2d40
0xBGoat <bgoatcrypto@protonmail.com>
2022-07-18 23:24:06 -0500

    Match syntax breaking things for Python < 3.10 users

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Easy CloneX",
     "author": "OxBGoat",
-    "version": (0, 1, 6),
+    "version": (0, 1, 7),
     "blender": (3, 2, 0),
     "description": "An Add-on that makes it easy to assemble a Clone from CloneX 3D files",
     "category": "CloneX"
